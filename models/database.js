910bd11dea41a1c8ce1d903bc1db1690a182b0b9
Shruti Vaishy <145763367+Shruti-vaishy@users.noreply.github.com>
2024-07-04 18:49:35 +0530

    Update database.js

--- models/database.js
+++ models/database.js
@@ -1,6 +1,6 @@
 const mongoose = require("mongoose")
 
-mongoose.connect("mongodb://127.0.0.1:27017/bookdirectory")
+mongoose.connect("mongodb+srv://shrutivaishy610:Kushagra@2107@cluster0.6bxwfsi.mongodb.net/bookdirectory")
 .then(()=>console.log('Connected to the Database'))
 .catch(err=>console.log(err));
 
